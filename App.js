abe0015cc0145cdd2c40eed1ddb1e77a7ebda09b
Mark <markconanan@digital8.com.au>
2019-02-27 09:56:44 +1000

    Made slider and steppers

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
+  state = {
+    value: 0
+  }
+
   render() {
     return (
       <View>
